5b5382d831e5acc9cb6de262cec052d53104f0cc
Gabriel Torres <gabrielstorres08@gmail.com>
2025-04-09 08:50:59 -0300

    Update LinkedList.java
    Include setNode, insert and remove by index, reverse

--- Single-LinkedList/src/LinkedList.java
+++ Single-LinkedList/src/LinkedList.java
@@ -103,5 +103,62 @@ public class LinkedList{
       }
       return temp;
     }
-    
+      public boolean setNode(int index, int value){
+      Node temp = getNode(index);
+      if (temp != null) {
+          temp.value = value;
+          return true;
+      }
+      return false;
+  }
+  
+    public boolean insert(int index, int value){
+      if (index < 0 || index > length){
+         return false;
+      }
+      if (index == 0){
+          prepend(value);
+          return true;
+      }
+      if (index == length){
+          append(value);
+          return true;
+      }
+      Node newNode = new Node(value);
+      Node temp = getNode(index - 1);
+      newNode.next = temp.next;
+      temp.next = newNode;
+      length++;
+      return true;   
+  }
+    public Node remove(int index){
+          if(index < 0 || index > length){
+              return null;
+          }
+          if(index == 0){
+              return removeFirst();
+          }
+          if (index == length - 1){
+              return removeLast();
+          }
+          Node prev = getNode(index - 1);
+          Node temp = prev.next;
+          prev.next = temp.next;
+          temp.next = null;
+          length--;
+          return temp;
+      }
+    public void reverse(){
+          Node temp = head;
+          head = tail;
+          tail = temp;
+          Node after = temp.next;
+          Node before = null;
+          for (int i = 0; i < length; i++){
+              after = temp.next;
+              temp.next = before;
+              before = temp;
+              temp = after;
+          }
+      }
   }
