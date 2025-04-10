2fac4e2b92700cb3830ecd0c2be364dea6b66c38
Gabriel Torres <gabrielstorres08@gmail.com>
2025-04-10 18:14:49 -0300

    Add files via upload

---
+++ Doubly-Linked-List/src/Main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        DoublyLinkedList myDLL = new DoublyLinkedList(7);
+        
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
+        myDLL.printList();
+   }
+}
