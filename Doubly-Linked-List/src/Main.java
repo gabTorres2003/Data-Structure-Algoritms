1859f5c30d2333d52c28bb458a963cc724381fd3
Gabriel Torres <gabrielstorres08@gmail.com>
2025-04-12 14:22:51 -0300

    Update Main.java

--- Doubly-Linked-List/src/Main.java
+++ Doubly-Linked-List/src/Main.java
@@ -1,4 +1,4 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         DoublyLinkedList myDLL = new DoublyLinkedList(7);
         
@@ -6,5 +6,20 @@ public class main {
         myDLL.getTail();
         myDLL.getLength();
         myDLL.printList();
+        
+        System.out.println();
+        myDLL.append(8);
+        myDLL.printList();
+        myDLL.getLength();
+        
+        System.out.println();
+        myDLL.removeLast();
+        myDLL.printList();
+        myDLL.getLength();
+        
+        System.out.println();
+        myDLL.prepend(6);
+        myDLL.printList();
+        myDLL.getLength();
    }
 }
