d91244961e0673edcf0dfce01c2b35135501569c
Gabriel Torres <gabrielstorres08@gmail.com>
2025-04-23 15:00:23 -0300

    Update DoublyLinkedList.java

--- Doubly-Linked-List/src/DoublyLinkedList.java
+++ Doubly-Linked-List/src/DoublyLinkedList.java
@@ -145,15 +145,16 @@ public class DoublyLinkedList{
     public Node remove(int index){
         if(index < 0 || index >= length) return null;
         if(index == 0) return removeFirst();
-        if(index == lenght - 1) return removeLast();
-        if(length == 1){
+        if(index == length - 1) return removeLast();
+        
         Node temp  = get(index);
-        Node before = get(index-1);
-        Node after = temp.next;
-        before.next= after;
-        after.prev = before;
+        
+        temp.next.prev = temp.prev;
+        temp.prev.next = temp.next;
         temp.next = null;
         temp.prev = null;
+        
+        length--;
         return temp;
     }
 }
