1f9dba718217d5a653be5b2ded2c593298a5a434
Gabriel Torres <gabrielstorres08@gmail.com>
2025-04-23 13:57:00 -0300

    Add files via upload

---
+++ Binary-Search-Tree/src/BinarySearchTree.java
@@ -0,0 +1,14 @@
+ublic class BinarySearchTree{
+    
+    Node root;
+    
+    class Node{
+        int value;
+        Node left;
+        Node right;
+        
+        Node(int value){
+            this.value = value;
+        }
+    }
+}
