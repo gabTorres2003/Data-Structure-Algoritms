46289d63c731c8319b47d522446aaab251fbea4b
Gabriel Torres <gabrielstorres08@gmail.com>
2025-04-24 00:32:54 -0300

    Update Main.java

--- Binary-Search-Tree/src/Main.java
+++ Binary-Search-Tree/src/Main.java
@@ -2,6 +2,17 @@ public class Main{
     public static void main(String[] args) {
         BinarySearchTree myBST = new BinarySearchTree();
         
-        System.out.println("Root: " + myBST.root);
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        myBST.insert(27);
+
+        //Espera-se o 27 ao executar
+        
+        System.out.println(myBST.root.left.right.value);
    }
 }
