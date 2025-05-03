727cfb8446b625a41e68294bac38bf330bb089b6
Gabriel Torres <gabrielstorres08@gmail.com>
2025-05-03 17:02:07 -0300

    Update Main.java

--- Hash-Table/src/Main.java
+++ Hash-Table/src/Main.java
@@ -3,6 +3,13 @@ public class Main {
         
         HashTable myHT = new HashTable();
         
+        myHT.set("nails", 100);
+        myHT.set("tile", 50);
+        myHT.set("lumber", 80);
+        
+        myHT.set("bolts", 200);
+        myHT.set("screws", 140);
+        
         myHT.printHashTable();
    }
 }
